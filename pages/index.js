24516233c21bbad0f0dd18f210802c1667386d6c
Jhumar <101704823+jhumar1@users.noreply.github.com>
2022-04-01 15:53:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,16 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-        Ok <code>pages/index.js</code>
+      When buying <a href="https://www.sandblast.in/"> Sand Blasting Machine </a>, make sure you know the advantages and disadvantages
+of each. The machine must be able to perform all tasks effectively. A good machine should be
+easy to clean. It should be easy to move parts and is designed to be mobile. A good
+sandblasting machine should be able to move heavy parts. In addition, it should be easy to use
+for the operator. It should be lightweight, durable, and easy to maneuver.
+Choose a machine that can perform multiple tasks. A sandblasting machine should be easy to
+use and maintain. It should also be easy to transport. If you are going to use the machine for
+industrial purposes, you must consider a machine with wheels. In this way, you will be able to
+maneuver the machine easily. However, it is important to buy the right sandblasting machine for
+your business. It will also help you save money.
         </p>
       </main>
 
